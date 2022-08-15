e95d50ef72b18d9418d4076a31933a52eb43d69a
TA <ta@bgn.games>
2022-08-15 08:36:06 -0400

    commit database model

---
+++ src/api/blockchain-game/controllers/blockchain-game.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ *  blockchain-game controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::blockchain-game.blockchain-game');
---
+++ src/api/blockchain-game/routes/blockchain-game.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * blockchain-game router.
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::blockchain-game.blockchain-game');
